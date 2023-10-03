b52e81894027fabf474240e13acf832b579c38a5
Lucia Mejia <117280922+luciamejiav@users.noreply.github.com>
2023-10-03 12:48:21 +0200

    Add files via upload

---
+++ 01_espar/Par.cs
@@ -0,0 +1,38 @@
+﻿// crear en la terminal dotnet new console
+using System;
+using Microsoft.VisualBasic;
+
+namespace _01_espar
+{ //poner el mismo nombre que en el RootNamespace 
+    internal class Par
+    {
+        static void Main(string[] args)
+        {
+            string? numero;
+            int n;
+
+            Console.Write("Introduce un número: ");
+            numero = Console.ReadLine();
+
+            try
+            {
+                n = Int32.Parse(numero); //pasamos el nº de string a int
+                Console.WriteLine(n);
+                if (n % 2 == 0) //comprobamos si es par
+                {
+                    Console.WriteLine("Es par");
+                }
+                else
+                {
+                    Console.WriteLine("Es impar");
+                }
+            }
+            catch (FormatException) //si no es entero salta un aviso
+            {
+                Console.WriteLine($"Unable to parse '{numero}'");
+            }
+
+
+        }
+    }
+}
